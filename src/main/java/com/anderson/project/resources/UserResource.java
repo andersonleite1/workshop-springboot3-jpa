2071eb83efb30b6c24f8a65813d5707837e5ce0f
Anderson Leite <andersonleite.dev@gmail.com>
2023-02-23 12:12:26 -0300

    feat(resources): add method findById at UserResource

--- src/main/java/com/anderson/project/resources/UserResource.java
+++ src/main/java/com/anderson/project/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class UserResource {
 	public ResponseEntity<List<User>> findAll() {
 		List<User> userList = userService.findAll();
 		return ResponseEntity.ok().body(userList);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
