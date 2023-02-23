9023a6be1b659f6c55c87c72901cc0ef618473ea
Anderson Leite <andersonleite.dev@gmail.com>
2023-02-23 11:41:28 -0300

    rafactor(repositories): add annotation @Repository at UserRepository

--- src/main/java/com/anderson/project/repositories/UserRepository.java
+++ src/main/java/com/anderson/project/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.anderson.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.anderson.project.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
