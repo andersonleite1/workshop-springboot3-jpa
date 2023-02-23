76056beaf2a4f79553cf882651d7c4e9b3bc957d
Anderson Leite <andersonleite.dev@gmail.com>
2023-02-23 11:51:05 -0300

    feat(resources): add methord findAll at UserResource

--- src/main/java/com/anderson/project/resources/UserResource.java
+++ src/main/java/com/anderson/project/resources/UserResource.java
@@ -1,18 +1,25 @@
 package com.anderson.project.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anderson.project.entities.User;
+import com.anderson.project.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	@Autowired
+	private UserService userService;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user1 = new User(1, "joao", "jao@email.com", "11223344", "user123");
-		return ResponseEntity.ok().body(user1);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> userList = userService.findAll();
+		return ResponseEntity.ok().body(userList);
 	}
 }
