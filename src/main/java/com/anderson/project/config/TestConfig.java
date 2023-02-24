71455a929edab54b5154e3f8fb9fbff1bc460d99
Anderson Leite <andersonleite.dev@gmail.com>
2023-02-24 08:49:34 -0300

    feat(config) add orderStatus at seeding for population orders

--- src/main/java/com/anderson/project/config/TestConfig.java
+++ src/main/java/com/anderson/project/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.anderson.project.entities.Order;
 import com.anderson.project.entities.User;
+import com.anderson.project.entities.enums.OrderStatus;
 import com.anderson.project.repositories.OrderRepository;
 import com.anderson.project.repositories.UserRepository;
 
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
 		User user1 = new User(null, "Carla", "carla@email.com", "11223344", "pass1234");
 		User user2 = new User(null, "Pedro", "pedro@email.com", "99887766", "user1234");
 
-		Order order1 = new Order(null, Instant.parse("2022-06-20T19:53:07Z"), user1);
-		Order order2 = new Order(null, Instant.parse("2022-07-21T03:42:10Z"), user2);
-		Order order3 = new Order(null, Instant.parse("2022-07-22T15:21:22Z"), user1);
+		Order order1 = new Order(null, Instant.parse("2022-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+		Order order2 = new Order(null, Instant.parse("2022-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+		Order order3 = new Order(null, Instant.parse("2022-07-22T15:21:22Z"), OrderStatus.SHIPPED, user1);
 
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
