6b6611da42d2d5986336f5bb9fbf6e8e8fc72ae8
Anderson Leite <andersonleite.dev@gmail.com>
2023-02-25 11:47:07 -0300

    feat(config): add seedind for population products

--- src/main/java/com/anderson/project/config/TestConfig.java
+++ src/main/java/com/anderson/project/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.anderson.project.entities.Category;
 import com.anderson.project.entities.Order;
+import com.anderson.project.entities.Product;
 import com.anderson.project.entities.User;
 import com.anderson.project.entities.enums.OrderStatus;
 import com.anderson.project.repositories.CategoryRepository;
 import com.anderson.project.repositories.OrderRepository;
+import com.anderson.project.repositories.ProductRepository;
 import com.anderson.project.repositories.UserRepository;
 
 @Configuration
@@ -27,6 +29,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -40,9 +45,16 @@ public class TestConfig implements CommandLineRunner {
 		Category category1 = new Category(null, "Electronics");
 		Category category2 = new Category(null, "Books");
 		Category category3 = new Category(null, "Computers");
+		
+		Product product1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product product2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product product3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product product4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product product5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
 		categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
+		productRepository.saveAll(Arrays.asList(product1, product2, product3, product4, product5));
 	}
 }
