9e201f88dc74de39eab37c383e5b2070e3ebb354
Anderson Leite <andersonleite.dev@gmail.com>
2023-02-25 10:23:51 -0300

    feat(config): add seedind for population categories

--- src/main/java/com/anderson/project/config/TestConfig.java
+++ src/main/java/com/anderson/project/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.anderson.project.entities.Category;
 import com.anderson.project.entities.Order;
 import com.anderson.project.entities.User;
 import com.anderson.project.entities.enums.OrderStatus;
+import com.anderson.project.repositories.CategoryRepository;
 import com.anderson.project.repositories.OrderRepository;
 import com.anderson.project.repositories.UserRepository;
 
@@ -22,6 +24,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -31,8 +36,13 @@ public class TestConfig implements CommandLineRunner {
 		Order order1 = new Order(null, Instant.parse("2022-06-20T19:53:07Z"), OrderStatus.PAID, user1);
 		Order order2 = new Order(null, Instant.parse("2022-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
 		Order order3 = new Order(null, Instant.parse("2022-07-22T15:21:22Z"), OrderStatus.SHIPPED, user1);
+		
+		Category category1 = new Category(null, "Electronics");
+		Category category2 = new Category(null, "Books");
+		Category category3 = new Category(null, "Computers");
 
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
+		categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
 	}
 }
