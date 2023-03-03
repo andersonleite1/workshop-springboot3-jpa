f5b9becf4f6ba485c63fc5e7160dfe62a2319c3f
Anderson Leite <andersonleite.dev@gmail.com>
2023-03-03 20:33:33 -0300

    feat(resources): add method create user

--- src/main/java/com/anderson/project/resources/UserResource.java
+++ src/main/java/com/anderson/project/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.anderson.project.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.anderson.project.entities.User;
 import com.anderson.project.services.UserService;
@@ -28,5 +32,12 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Integer id) {
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> create(@RequestBody User user) {
+		user = userService.create(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
 	}
 }
